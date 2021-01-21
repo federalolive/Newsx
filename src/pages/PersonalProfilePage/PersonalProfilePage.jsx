a9e4b374b4537f4e161698466f0ff7cdd09d2cd3
kimjane93 <kjchadwell93@gmail.com>
2021-01-21 14:03:19 -0500

    fix profile

--- src/pages/PersonalProfilePage/PersonalProfilePage.jsx
+++ src/pages/PersonalProfilePage/PersonalProfilePage.jsx
@@ -24,7 +24,6 @@ class PersonalProfilePage extends Component {
 
     render() { 
         const user = this.state.user
-        console.log(user.articleCollection)
         const userArticleCollection = this.state.user.articleCollection
         
         return ( 
@@ -60,16 +59,5 @@ class PersonalProfilePage extends Component {
 }
  
 export default PersonalProfilePage;
-
-
-
-// Class component: needs to hold state so we can delete, and read
-
-// map over userColelction, and render Article Cards for each item.
-
-
-// render bio, avatar, if you're signed in, and you're you, render inputs and buttons to update bio and image url
-
-
 
 
